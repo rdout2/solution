187924f09e9b9729f60b3303937acd20a6a114ae
ahmat1 <rahmatmahamatdout@gmail.com>
2025-05-22 12:11:39 +0200

    fix docker compose

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()  # Charge le .env automatiquement
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
