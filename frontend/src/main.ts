fc2e8c058c1ab401cd2ce3c54bf462449e28d85a
ahmat1 <rahmatmahamatdout@gmail.com>
2025-05-22 14:06:45 +0200

    fix .env

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { clerkPlugin } from '@clerk/vue'
 
-createApp(App).use(router).mount('#app')
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+// createApp(App).use(router).mount('#app')
+
+const app = createApp(App)
+app.use(router)
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY })
+app.mount('#app')
